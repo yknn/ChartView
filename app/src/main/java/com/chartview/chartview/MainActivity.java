2611647fda6df9a0e046ee365f5815512cb89596
yukun <yk_coding@163.com>
2018-07-25 15:06:59 +0800

    下班提交

--- app/src/main/java/com/chartview/chartview/MainActivity.java
+++ app/src/main/java/com/chartview/chartview/MainActivity.java
@@ -1,13 +1,10 @@
 package com.chartview.chartview;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
 
 import com.chartview.view.LineWPChartView;
 
@@ -19,18 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
         lin = findViewById(R.id.line);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               lin.setXName("我是X轴名称").commite();
-            }
-        });
     }
 
 
